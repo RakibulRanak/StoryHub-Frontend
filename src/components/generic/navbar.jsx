545e6b61ba0bdd2af8d41cf3802902b4d585c6a0
RakibulRanak <rakibulhasanranak1@gmail.com>
2021-11-29 13:55:27 +0600

    fix navbar responsiveness

--- src/components/generic/navbar.jsx
+++ src/components/generic/navbar.jsx
@@ -1,21 +1,23 @@
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
 import {
     Button,
     Flex,
     Heading,
+    IconButton,
     Link,
+    Switch,
     Text,
     useColorMode,
     useColorModeValue
 } from '@chakra-ui/react';
 import { useContext, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import Switch from 'react-switch';
 import { AuthContext } from '../../context/authContext';
 
 const MyNavbar = (props) => {
     const { loggedIn, logoutHandler, user } = useContext(AuthContext);
     const [checked, setChecked] = useState(JSON.parse(localStorage.getItem('dark_theme')) || false);
-
+    const [display, changeDisplay] = useState('none');
     const { colorMode, toggleColorMode } = useColorMode();
     const navColor = useColorModeValue('gray.800', 'gray.500');
     const color = useColorModeValue('white', 'gray.800');
@@ -40,45 +42,115 @@ const MyNavbar = (props) => {
     );
 
     return (
-        <Flex
-            as="nav"
-            align="center"
-            justify="space-between"
-            wrap="wrap"
-            padding="0.8rem"
-            boxShadow="2xl"
-            bg={navColor}
-            color="white"
-            {...props}
-        >
-            <Flex align="center" mr={0}>
-                <Heading as="h1" size="lg" letterSpacing="-.1rem">
-                    <MenuItems to="/">StoryHub</MenuItems>
-                </Heading>
+        <Flex top="1rem" right="1rem" overflowX="hidden">
+            <Flex align="center" width="100vw" bgColor={navColor}>
+                <Flex
+                    display={['none', 'none', 'flex', 'flex']}
+                    w="100vw"
+                    mt={0}
+                    position="relative"
+                    align="center"
+                    justify="space-between"
+                    padding="0.8rem"
+                    boxShadow="2xl"
+                    bg={navColor}
+                    color="white"
+                >
+                    <Flex align="center" mr={0}>
+                        <Heading as="h1" size="lg" letterSpacing="-.1rem">
+                            <MenuItems to="/">StoryHub</MenuItems>
+                        </Heading>
+                    </Flex>
+                    <Switch ml="2px" pl="1px" onChange={handleChange} isChecked={checked} />
+                    {user && (
+                        <Button boxShadow="none !important" colorScheme="white" ml="auto" mr="0">
+                            <Text fontSize="17px" color="white">
+                                {user.username}
+                            </Text>
+                        </Button>
+                    )}
+                    {!loggedIn && (
+                        <Button as={RouterLink} to="/signin" bg="transparent" border="1px" mr={5}>
+                            Log In
+                        </Button>
+                    )}
+                    {loggedIn && (
+                        <Button onClick={logoutHandler} bg={navColor} border="1px" mr={5}>
+                            Log Out
+                        </Button>
+                    )}
+                </Flex>
+                {display === 'none' && (
+                    <IconButton
+                        aria-label="Open Menu"
+                        size="lg"
+                        m={2}
+                        p={5}
+                        icon={<HamburgerIcon />}
+                        onClick={() => changeDisplay('flex')}
+                        display={['flex', 'flex', 'none', 'none']}
+                        zIndex={3}
+                    />
+                )}
             </Flex>
-            {/* <Button onClick={toggleColorMode}>
-                Toggle {colorMode === 'light' ? 'white' : 'gray.800'}
-            </Button> */}
+            <Flex
+                w="100vw"
+                display={display}
+                bgColor={navColor}
+                color="white"
+                zIndex={20}
+                pos="relative"
+                top="0"
+                left="0"
+                overflow="none"
+                direction="column"
+            >
+                <Flex justify="flex-end">
+                    <IconButton
+                        mt={2}
+                        mr={2}
+                        aria-label="Open Menu"
+                        size="lg"
+                        icon={<CloseIcon />}
+                        onClick={() => changeDisplay('none')}
+                    />
+                </Flex>
 
-            <Switch ml="2px" pl="1px" onChange={handleChange} checked={checked} />
-
-            {user && (
-                <Button boxShadow="none !important" colorScheme="white" ml="auto" mr="0">
-                    <Text fontSize="17px" color="white">
-                        {user.username}
-                    </Text>
-                </Button>
-            )}
-            {!loggedIn && (
-                <Button as={RouterLink} to="/signin" bg="transparent" border="1px">
-                    Log In
-                </Button>
-            )}
-            {loggedIn && (
-                <Button onClick={logoutHandler} bg="transparent" border="1px">
-                    Log Out
-                </Button>
-            )}
+                <Flex direction="column" align="center" width="100vw" pb="4">
+                    {user && (
+                        <Flex align="center">
+                            <Button boxShadow="none !important" colorScheme="white">
+                                <Text fontSize="17px" color="white">
+                                    {user.username}
+                                </Text>
+                            </Button>
+                        </Flex>
+                    )}
+                    {!loggedIn && (
+                        <Flex>
+                            <Button
+                                mb={3}
+                                as={RouterLink}
+                                to="/signin"
+                                bg="transparent"
+                                border="1px"
+                            >
+                                Log In
+                            </Button>
+                        </Flex>
+                    )}
+                    {loggedIn && (
+                        <Flex>
+                            <Button mb={3} onClick={logoutHandler} bg={navColor} border="1px">
+                                Log Out
+                            </Button>
+                        </Flex>
+                    )}
+                    <Flex>
+                        <Switch onChange={handleChange} isChecked={checked} />
+                    </Flex>
+                </Flex>
+            </Flex>
         </Flex>
     );
 };
