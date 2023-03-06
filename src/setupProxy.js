e397b30826daceed34d2993b83ce392536d403ef
RakibulRanak <rakibulhasanranak1@gmail.com>
2023-03-06 12:33:41 +0600

    no major changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,7 @@
 const  { createProxyMiddleware } = require("http-proxy-middleware");
 require('dotenv').config()
 const URL = process.env.REACT_APP_URL
+console.log(URL)
 module.exports = function(app) {
   app.use(
     '/api',
